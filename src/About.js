3779e8ec9278736f9d9f3273f9d281624f17e028
SHREYA SHASTRY <shreyashastry@SHREYAs-MacBook-Air-2.local>
2023-11-07 14:38:36 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="section">
+      <h2>About Me</h2>
+      <p>I am Shreya Shastry, a third year B.Tech student studying at VIT University, Vellore. Driven by a thirst of knowledge and innovation, I am enthusiastic about applying my skills to real-world challenges. 
+  My goal is to become a proficient technologist and make impactful contributions to the ever-evolving field of information technology.
+ </p>
+
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="section">
+      <h2>Contact Me</h2>
+      <p>Email: shastry918@gmail.com</p>
+      <p>Phone: 8459839776</p>
+      <p>LinkedIn: www.linkedin.com/in/shreya-shastry-a92b44232</p>
+    </div>
+  );
+};
+
+export default Contact;
